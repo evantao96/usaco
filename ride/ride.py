c498fe4a79fe2729e2d76e03644cfbaf7271a3d4
evantaoo <52547607+evantaoo@users.noreply.github.com>
2022-06-05 20:19:36 -0400

    Update ride.py

--- ride/ride.py
+++ ride/ride.py
@@ -3,8 +3,10 @@ PROG: ride
 LANG: PYTHON3
 """
 
-comet = input()
-line = input()
+fin = open('ride.in', 'r')
+comet = fin.readline().strip()
+line = fin.readline().strip()
+fin.close()
 
 letters = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
 prod_c = 1
@@ -16,6 +18,10 @@ for l in line:
 	prod_l = prod_l * letters[l]
 
 if prod_c % 47 == prod_l % 47: 
-	print('GO')
+	ans = 'GO'
 else: 
-	print('STAY')
+	ans = 'STAY'
+
+fout = open('ride.out', 'w')
+fout.write(ans + '\n')
+fout.close()
